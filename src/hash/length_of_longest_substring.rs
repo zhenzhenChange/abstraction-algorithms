2ccab12e511614aa1b97cb5c689521cfaa1ff6ba
zhongzhen-wsl <zhongzhen-wsl>
2023-06-29 18:38:22 +0800

    ✈️ length_of_longest_substring

---
+++ src/hash/length_of_longest_substring.rs
@@ -0,0 +1,66 @@
+#![allow(unused)]
+
+/// `T-O(N)`
+///
+/// `S-O(|Σ|) => |Σ| => ASCII 128`
+///
+/// https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+pub fn solution(s: String) -> i32 {
+    let mut ret = 0;
+    let mut slow = 0;
+    let mut fast = 0;
+    let mut hashmap = std::collections::HashMap::new();
+
+    for c in s.chars() {
+        if let Some(i) = hashmap.get(&c) {
+            slow = std::cmp::max(slow, i + 1);
+        }
+
+        hashmap.insert(c, fast as i32);
+        fast += 1;
+
+        ret = std::cmp::max(ret, fast - slow)
+    }
+
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solution;
+
+    #[test]
+    fn case1() {
+        let ret = solution(String::from("abcabcbb"));
+
+        assert_eq!(ret, 3)
+    }
+
+    #[test]
+    fn case2() {
+        let ret = solution(String::from("bbbbb"));
+
+        assert_eq!(ret, 1)
+    }
+
+    #[test]
+    fn case3() {
+        let ret = solution(String::from("pwwkew"));
+
+        assert_eq!(ret, 3)
+    }
+
+    #[test]
+    fn case4() {
+        let ret = solution(String::from("abba"));
+
+        assert_eq!(ret, 2)
+    }
+
+    #[test]
+    fn case5() {
+        let ret = solution(String::from(""));
+
+        assert_eq!(ret, 0)
+    }
+}
--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -1,1 +1,2 @@
+mod length_of_longest_substring;
 mod two_sum;
