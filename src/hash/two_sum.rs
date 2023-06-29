3ec25cfb61acec24abc2dd02cb413bdb12c70ef5
zhongzhen-wsl <zhongzhen-wsl>
2023-06-29 15:57:36 +0800

    ✈️ two_sum

---
+++ src/hash/two_sum.rs
@@ -0,0 +1,53 @@
+#![allow(unused)]
+
+use std::collections::HashMap;
+
+/// `T-O(N)`
+///
+/// `S-O(N)`
+///
+/// https://leetcode.cn/problems/two-sum/
+pub fn solution(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map = HashMap::new();
+
+    for (i, num) in nums.into_iter().enumerate() {
+        let diff = target - num;
+
+        if let Some(&cached) = map.get(&diff) {
+            return vec![i as i32, cached];
+        }
+
+        map.insert(num, i as i32);
+    }
+
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solution;
+
+    #[test]
+    fn case1() {
+        let mut ret = solution(vec![2, 7, 11, 15], 9);
+        ret.sort();
+
+        assert_eq!(ret, vec![0, 1])
+    }
+
+    #[test]
+    fn case2() {
+        let mut ret = solution(vec![3, 2, 4], 6);
+        ret.sort();
+
+        assert_eq!(ret, vec![1, 2])
+    }
+
+    #[test]
+    fn case3() {
+        let mut ret = solution(vec![3, 3], 6);
+        ret.sort();
+
+        assert_eq!(ret, vec![0, 1])
+    }
+}
