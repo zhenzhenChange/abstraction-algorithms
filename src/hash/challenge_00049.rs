944dfb216686104b06920a322806a53630da2f75
zhongzhen-wsl <zhongzhen-wsl>
2023-07-18 11:39:21 +0800

    ✈️ group_anagrams

--- src/hash/challenge_00049.rs
+++ src/hash/challenge_00049.rs
@@ -1,19 +1,31 @@
 /// 00049
 ///
-/// `T-O()`
+/// `T-O(N * K * logK)`
 ///
-/// `S-O()`
+/// `S-O(N * K)`
 ///
 /// https://leetcode.cn/problems/group-anagrams/
 #[allow(unused)]
 pub fn solution(strs: Vec<String>) -> Vec<Vec<String>> {
-    vec![]
+    let mut map = std::collections::HashMap::new();
+
+    for string in strs {
+        // NOTE: 排序字符
+        let mut chars = string.chars().collect::<Vec<_>>();
+        chars.sort();
+
+        // NOTE: 以排序重组后的字符串作为 key 存储当前字符串
+        let sorted = chars.iter().collect::<String>();
+        map.entry(sorted).or_insert_with(Vec::new).push(string);
+    }
+
+    map.values().map(|v| v.clone()).collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::solution;
-    use crate::helpers::vec::as_string;
+    use crate::helpers::vec::{as_string, sort_2d};
 
     #[test]
     fn case1() {
@@ -24,7 +36,7 @@ mod tests {
             as_string(vec!["ate", "eat", "tea"]),
         ];
 
-        assert_eq!(solution(preset_arg), preset_ret);
+        assert_eq!(sort_2d(preset_ret), sort_2d(solution(preset_arg)));
     }
 
     #[test]
@@ -32,7 +44,7 @@ mod tests {
         let preset_arg = as_string(vec![""]);
         let preset_ret = vec![as_string(vec![""])];
 
-        assert_eq!(solution(preset_arg), preset_ret);
+        assert_eq!(sort_2d(preset_ret), sort_2d(solution(preset_arg)));
     }
 
     #[test]
@@ -40,6 +52,6 @@ mod tests {
         let preset_arg = as_string(vec!["a"]);
         let preset_ret = vec![as_string(vec!["a"])];
 
-        assert_eq!(solution(preset_arg), preset_ret);
+        assert_eq!(sort_2d(preset_ret), sort_2d(solution(preset_arg)));
     }
 }
--- src/helpers/vec.rs
+++ src/helpers/vec.rs
@@ -1,4 +1,9 @@
-pub fn compare() {}
+pub fn sort_2d(mut matrix: Vec<Vec<String>>) -> Vec<Vec<String>> {
+    matrix.sort_by_key(|v| v.len());
+    matrix.iter_mut().for_each(|v| v.sort());
+
+    matrix
+}
 
 pub fn as_string(str_vec: Vec<&str>) -> Vec<String> {
     str_vec
