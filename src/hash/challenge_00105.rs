827e552a4654c368175fce381fd1775f927489da
zhongzhen-bab <1506678296@qq.com>
2023-08-04 16:01:35 +0800

    ✈️ build_tree

--- src/hash/challenge_00105.rs
+++ src/hash/challenge_00105.rs
@@ -1,17 +1,29 @@
-use std::{cell::RefCell, rc::Rc};
-
-use crate::helpers::tree::TreeNode;
+use crate::helpers::tree::{SubTree, TreeNode};
 
 /// 00105
 ///
-/// `T-O()`
+/// `T-O(N)`
 ///
-/// `S-O()`
+/// `S-O(N)`
 ///
 /// https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 #[allow(unused)]
-pub fn solution(pre_order: Vec<i32>, in_order: Vec<i32>) -> Option<Rc<RefCell<TreeNode<i32>>>> {
-    Some(Rc::new(RefCell::new(TreeNode::new(0))))
+pub fn solution(mut pre_order: Vec<i32>, mut in_order: Vec<i32>) -> SubTree<i32> {
+    if pre_order.is_empty() || in_order.is_empty() {
+        return None;
+    }
+
+    let root = pre_order.remove(0);
+    let root_idx = in_order.iter().position(|&node| node == root).unwrap();
+    let root_node = TreeNode::new(root).into_ref();
+
+    let (in_r, in_l) = (in_order.split_off(root_idx + 1), in_order);
+    let (pre_r, pre_l) = (pre_order.split_off(root_idx), pre_order);
+
+    root_node.borrow_mut().left = solution(pre_l, in_l);
+    root_node.borrow_mut().right = solution(pre_r, in_r);
+
+    Some(root_node)
 }
 
 #[cfg(test)]
@@ -25,7 +37,7 @@ mod tests {
         let preset_arg0 = vec![3, 9, 20, 15, 7];
         let preset_arg1 = vec![9, 3, 15, 20, 7];
 
-        let preset_ret = TreeNode::from(vec![
+        let preset_ret = TreeNode::from_bfs(vec![
             Some(3),
             Some(9),
             Some(20),
@@ -43,7 +55,7 @@ mod tests {
         let preset_arg0 = vec![-1];
         let preset_arg1 = vec![-1];
 
-        let preset_ret = TreeNode::from(vec![Some(-1)]);
+        let preset_ret = TreeNode::from_bfs(vec![Some(-1)]);
 
         assert_eq!(preset_ret, solution(preset_arg0, preset_arg1));
     }
--- src/helpers/tree.rs
+++ src/helpers/tree.rs
@@ -1,8 +1,16 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, collections::VecDeque, fmt::Debug, rc::Rc};
 
-type SubTree<T> = Option<Rc<RefCell<TreeNode<T>>>>;
+pub type SubTree<T> = Option<RefTree<T>>;
+pub type RefTree<T> = Rc<RefCell<TreeNode<T>>>;
 
-impl<T: Clone + Default> TreeNode<T> {
+#[derive(Debug, Eq, PartialEq)]
+pub struct TreeNode<T> {
+    pub val: T,
+    pub left: SubTree<T>,
+    pub right: SubTree<T>,
+}
+
+impl<T: Copy + Debug + Clone + Default> TreeNode<T> {
     #[inline]
     pub fn new(val: T) -> Self {
         Self {
@@ -13,21 +21,45 @@ impl<T: Clone + Default> TreeNode<T> {
     }
 
     #[inline]
-    pub fn from(v: Vec<Option<T>>) -> SubTree<T> {
-        if v.is_empty() {
+    pub fn into_ref(self) -> RefTree<T> {
+        Rc::new(RefCell::new(self))
+    }
+
+    #[inline]
+    pub fn from_bfs(mut bfs: Vec<Option<T>>) -> SubTree<T> {
+        if bfs.is_empty() {
             return None;
+        };
+
+        let root = Self::new(bfs.remove(0).unwrap()).into_ref();
+
+        let mut iter = bfs.into_iter().peekable();
+        let mut queue = VecDeque::from([root.clone()]);
+
+        while iter.peek().is_some() {
+            let head = queue.pop_front().unwrap();
+
+            if let Some(node) = iter.next() {
+                if let Some(node) = node {
+                    let node = Self::new(node).into_ref();
+
+                    // Left
+                    head.borrow_mut().left = Some(node.clone());
+                    queue.push_back(node);
+                }
+            }
+
+            if let Some(node) = iter.next() {
+                if let Some(node) = node {
+                    let node = Self::new(node).into_ref();
+
+                    // Right
+                    head.borrow_mut().right = Some(node.clone());
+                    queue.push_back(node);
+                }
+            }
         }
 
-        let node = Self::new(v[0].clone().unwrap());
-        let root = Some(Rc::new(RefCell::new(node)));
-
-        root
+        Some(root)
     }
-}
-
-#[derive(Debug, Eq, PartialEq)]
-pub struct TreeNode<T> {
-    pub val: T,
-    pub left: SubTree<T>,
-    pub right: SubTree<T>,
 }
